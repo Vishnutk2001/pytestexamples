c89ca29cc1aedb35cf1125fadb4bbb92a310c682
Vishnutk2001 <t.k.vishnu2001@gmail.com>
2022-03-29 10:43:48 +0530

    pytestexamples

--- Calculator_test.py
+++ Calculator_test.py
@@ -8,24 +8,21 @@ def test_add2no(x,y,z):
     res = Calculator.add2no(x,y)
     assert res == z
 
-@pytest.mark.parametrize("x,y,z",[(3,2,5),(5,1,6),(8,2,10),(5,2,9)])
+@pytest.mark.parametrize("x,y,z",[(20,30,40),(5,1,6),(8,2,10)])
 def test_sub2no(x,y,z):
-    x = 20
-    y = 10
+
     res = Calculator.sub2no(x, y)
     assert res == z
 
-@pytest.mark.parametrize("x,y,z",[(3,2,5),(5,1,6),(8,2,10),(5,2,9)])
+@pytest.mark.parametrize("x,y,z",[(3,2,5),(10,20,40),(8,2,10)])
 def test_mul2no(x,y,z):
-    x = 20
-    y = 10
+
     res = Calculator.mul2no(x, y)
     assert res == z
 
-@pytest.mark.parametrize("x,y,z",[(3,2,5),(5,1,6),(8,2,10),(5,2,9)])
+@pytest.mark.parametrize("x,y,z",[(3,2,5),(5,1,6),(20,30,40)])
 def test_div2no(x,y,z):
-    x = 20
-    y = 10
+
     res = Calculator.div2no(x, y)
     assert res == z
 
